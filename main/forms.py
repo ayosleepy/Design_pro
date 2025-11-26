91dd8eb9ccce3846043f8f7d3a310fbfe161406c
ayosleepy <filippmello2007@gmail.com>
2025-11-27 00:22:09 +0700

    guest

---
+++ main/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+import re
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    consent = forms.BooleanField(
+        required=True,
+        label='Согласие на обработку персональных данных'
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('fio', 'username', 'email', 'password1', 'password2', 'consent')
+        widgets = {
+            'fio': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'fio': 'ФИО',
+            'username': 'Логин',
+            'email': 'Email',
+        }
+
+    def clean_fio(self):
+        fio = self.cleaned_data.get('fio')
+        if not re.match(r'^[а-яА-ЯёЁ\s-]+$', fio):
+            raise forms.ValidationError('ФИО должно содержать только кириллические буквы, пробелы и дефисы')
+        return fio
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not re.match(r'^[a-zA-Z-]+$', username):
+            raise forms.ValidationError('Логин должен содержать только латинские буквы и дефисы')
+        return username
